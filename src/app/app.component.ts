94761b13f013f1d791e845c6cf4354e45b81928e
Somossy László <stony86@gmail.com>
2023-04-15 16:07:26 +0200

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'y';
+ 
+  num : number = 0;
+  numbers : Array<number> = new Array<number>();
+  
+  generateRandomNumber() {
+   this.num += Math.floor(Math.random() * 100) + 1;
+   if(this.num > Math.floor(Math.random() * 100) + 1) {
+    alert('A számítógép nagyobb számra gondolt!!!');
+   } else {
+    alert('A számítógép kisebb számra gondolt!!!');
+   
+  } if (this.num == Math.floor(Math.random() * 100) + 1 ) {
+    alert('A számítógép ugyan arra a számra gondolt!');  
+   }
+}   
+
+  setValue(x : HTMLInputElement) {
+    this.num = parseInt(x.value);
+    this.numbers.push(this.num);
+  }
 }
